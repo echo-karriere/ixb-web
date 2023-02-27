26797af79bcfd6a33be39c7e362671fd4961379f
helges1 <sebhelg@gmail.com>
2023-02-27 03:46:46 +0100

    Removed filtering

--- pages/for-studenter/stillingsannonser/index.tsx
+++ pages/for-studenter/stillingsannonser/index.tsx
@@ -21,15 +21,6 @@ const query = groq`*[_type == "joblisting" && defined(slug.current)]{
 
 export const getStaticProps = async () => {
   const data = await client.fetch(query);
-  // dont show if publish is false
-  const filteredData = data.filter(
-    (job: { publish: boolean }) => job.publish === true
-  );
-  // sort by deadline
-  filteredData.sort(
-    (a: { deadline: string }, b: { deadline: string }) =>
-      new Date(a.deadline).getTime() - new Date(b.deadline).getTime()
-  );
 
   return { props: { data } };
 };
