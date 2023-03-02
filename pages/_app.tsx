0ff8badae74199f33f4a5cfd16ab2c70dab1db9f
helges1 <sebhelg@gmail.com>
2023-03-02 22:31:19 +0100

    minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,51 +14,6 @@ Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <noscript>
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: 9999,
-            background: "white",
-            padding: "1rem",
-          }}>
-          <img
-            src="/webpageAssets/itxbergen.svg"
-            alt="ITxBergen logo"
-            style={{
-              width: "auto",
-              height: "2rem",
-            }}
-          />
-          <h1 style={{ fontSize: "2rem" }}>
-            Denne nettsiden krever JavaScript for å fungere.
-          </h1>
-          <p>
-            Vennligst aktiver JavaScript i nettleseren din for å fortsette til
-            nettsiden. Hvis du ikke vet hvordan du gjør dette, kan du finne
-            veiledning{" "}
-            <a
-              href="https://www.enablejavascript.io/no"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: "blue" }}>
-              her (ekstern lenke)
-            </a>
-            .
-          </p>
-          <p>
-            Har du ikke lyst til å aktivere JavaScript, men er likevel
-            interessert i å se hva vi driver med, kan du ta en titt på{" "}
-            <a href="/about-us.txt" style={{ color: "blue" }}>
-              denne siden
-            </a>
-          </p>
-        </div>
-      </noscript>
       <Layout>
         <Component {...pageProps} />
       </Layout>
