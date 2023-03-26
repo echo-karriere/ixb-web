9719189fafd4f188949df6977dd90aae0719e61d
helges1 <sebhelg@gmail.com>
2023-03-26 23:14:22 +0200

    Changes to the API

--- pages/api/getJobListings.ts
+++ pages/api/getJobListings.ts
@@ -1,4 +1,5 @@
 import { client } from "../../src/lib/sanity.client";
+import imageUrlBuilder from "@sanity/image-url";
 
 export default async function handler(
   req: any,
@@ -11,7 +12,7 @@ export default async function handler(
   }
 ) {
   const jobListings = await client.fetch(
-    `*[_type == "joblisting"]{slug, title, deadline, company, location, type}`
+    `*[_type == "joblisting"]{slug, title, deadline, company, location, type, logo}`
   );
   jobListings.sort(
     (
@@ -36,31 +37,39 @@ export default async function handler(
       return new Date(a.deadline).getTime() - new Date(b.deadline).getTime();
     }
   );
-  const jobListingsWithSlug = jobListings.map((job: { slug: any }) => {
+
+  const builder = imageUrlBuilder(client);
+  const jobListingsWithLogo = jobListings.map((job: { logo: any }) => {
+    return {
+      ...job,
+      logo: builder.image(job.logo).width(200).height(200).url(),
+    };
+  });
+
+  const jobListingsWithSlug = jobListingsWithLogo.map((job: { slug: any }) => {
     return {
       ...job,
       slug: job.slug.current,
     };
-  }); 
-
+  });
 
   res.status(200).json(jobListingsWithSlug);
 }
 
 export async function getStaticProps() {
   const jobListings = await client.fetch(
-    `*[_type == "joblisting"]{slug, title, deadline, company, location, type}`
+    `*[_type == "joblisting"]{slug, title, deadline, company, location, type, logo}`
   );
   const jobTitles = jobListings.map((job: { title: any }) => job.title);
   const jobDeadlines = jobListings.map(
     (job: { deadline: any }) => job.deadline
   );
 
-
   const slug = jobListings.map((job: { slug: any }) => job.slug.current);
-const type = jobListings.map((job: { type: any }) => job.type);
+  const type = jobListings.map((job: { type: any }) => job.type);
   const company = jobListings.map((job: { company: any }) => job.company);
   const location = jobListings.map((job: { location: any }) => job.location);
+  const logo = jobListings.map((job: { logo: any }) => job.logo);
 
   return {
     props: {
@@ -70,6 +79,7 @@ const type = jobListings.map((job: { type: any }) => job.type);
       company,
       location,
       type,
+      logo,
     },
   };
 }
