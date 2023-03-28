b013754a5221816c57003bf172baec49170483ef
helges1 <sebhelg@gmail.com>
2023-03-28 22:05:09 +0200

    CORS

--- next.config.js
+++ next.config.js
@@ -30,3 +30,26 @@ const nextConfig = {
 
 // Merge MDX config with Next.js config
 module.exports = withMDX(nextConfig);
+
+// Add CORS wildcard for API
+module.exports = {
+  ...module.exports,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
