dbff91a27485bdb1f061e1b8208ea9a8b3266bdf
helges1 <sebhelg@gmail.com>
2023-04-05 21:53:28 +0200

    Check if company is attending

--- components/for-bedrifter/pamelding/pamelding.tsx
+++ components/for-bedrifter/pamelding/pamelding.tsx
@@ -5,7 +5,6 @@ export default function PameldingInfo() {
     <div className="max-w-7xl w-11/12 mt-6 md:mt-10 mx-auto">
       <div className="max-w-3xl">
         <h1>Påmelding til ITxBERGEN sine karrieredager 2023</h1>
-
         {/* <div
           className="bg-yellow-100  rounded-b px-4 py-3 shadow-md"
           role="alert">
@@ -22,8 +21,6 @@ export default function PameldingInfo() {
             </span>
           </p>
         </div> */}
- 
-
         <p>
           Ønsker du og din bedrift å delta på våre karrieredager for
           IT-studenter i september? Påmeldingen er nå åpen! Du kan lese mer om
@@ -43,7 +40,64 @@ export default function PameldingInfo() {
           href="https://skjema.ixb.no/karrieredagene2023">
           Gå til påmeldingsskjema
         </a>
+        <br />
+        <br />
+        <h2>Usikker på om bedriften er påmeldt?</h2>
+        Om du ønsker å sjekke om bedriften din er påmeldt, kan du skrive inn
+        orginisasjonsnummeret nedenfor. Hvis bedriften er påmeldt, vil du få opp
+        en liste over hvilke dager bedriften er påmeldt på, samt hvilke
+        ekstratjenester bedriften har valgt.
       </div>
+      <br />
+      <form>
+        <label className="font-bold" htmlFor="ref">
+          Organisasjonsnummer
+        </label>
+        <br />
+        <input
+          className="border-gray-500 border p-2 border-solid"
+          type="text"
+          name="ref"
+          id="ref"
+          required
+          pattern="[0-9]{9}"
+          onInput={(e) => {
+            const input = e.target as HTMLInputElement; // add type annotation
+            input.value = input.value.replace(/[^0-9]/g, ""); // replace all non-numeric characters with empty string
+            if (input.value.length > 9) {
+              input.value = input.value.slice(0, 9);
+            }
+          }}
+        />
+
+        <br />
+        {/* Add error message element */}
+        <div
+          id="error-message"
+          style={{ color: "red", marginTop: "0.5rem" }}></div>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white p-2 mt-2 rounded"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault(); // prevent default form submission
+            const inputField = document.getElementById(
+              "ref"
+            ) as HTMLInputElement | null; // add type annotation and null check
+            const ref = inputField?.value; // add null check
+            const pattern = new RegExp(inputField?.pattern ?? ""); // add null check and create RegExp from pattern string
+            const errorMessage = document.getElementById(
+              "error-message"
+            ) as HTMLDivElement; // add type annotation and null check for error message element
+            if (!ref || !pattern.test(ref)) {
+              // check if ref is falsy or does not match pattern
+              errorMessage.innerText = "Ugyldig organisasjonsnummer."; // set error message text
+              return; // exit the function without redirecting
+            }
+            window.location.href = `https://skjema.ixb.no/status?ref=${ref}`; // append value to URL and redirect
+          }}>
+          Sjekk status
+        </button>
+      </form>
     </div>
   );
 }
