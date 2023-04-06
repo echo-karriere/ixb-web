90d3cde85b0de9783866bc2d3827b1a225cbe4d1
helges1 <sebhelg@gmail.com>
2023-04-06 16:48:03 +0200

    Design changes

--- components/index/events.tsx
+++ components/index/events.tsx
@@ -11,7 +11,7 @@ function Event({ event, index }: EventProps) {
   return (
     <Link href={`/arrangement/${event.slug.current}`} passHref>
       <div
-        className={`border-l-4 ${
+        className={`border-t-4 ${
           event.eventType === "Karrieredag"
             ? "border-red-700"
             : event.eventType === "Workshop"
@@ -19,7 +19,7 @@ function Event({ event, index }: EventProps) {
             : event.eventType === "Arrangement"
             ? "border-green-700"
             : "border-gray-700"
-        } p-4 m-4 bg-[#f2f2f2] hover:bg-gray-200`}
+        } p-4 m-4 bg-[#f2f2f2] hover:bg-gray-200 rounded`}
         key={event._id}>
         <h3 className="font-bold">{event.title}</h3>
         <p>
@@ -40,14 +40,20 @@ function Event({ event, index }: EventProps) {
 }
 
 export default function Events({ events }: { events: SanityDocument[] }) {
+  if (events.length === 0) {
+    return (
+      <div className="max-w-7xl w-11/12 mx-auto pt-6">
+        <h2 className="text-2xl font-bold ml-4">Arrangementer</h2>
+        <p className="text-gray-500 ml-4">Ingen arrangementer å vise 🙁</p>
+      </div>
+    );
+  }
   return (
     <div className="max-w-7xl w-11/12 mx-auto pt-6">
       <h2 className="text-2xl font-bold ml-4">Arrangementer</h2>
       <div className="flex flex-wrap">
         {events.map((event, index) => (
-          <div
-            key={event._id}
-            className={`w-full md:w-1/2 `}>
+          <div key={event._id} className={`w-full md:w-1/3 `}>
             <Event event={event} index={index} />
           </div>
         ))}
