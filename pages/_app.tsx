1dc386502260f5a8d415282550b7042585403d79
helges1 <sebhelg@gmail.com>
2023-08-04 19:03:22 +0200

    Minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
+import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/common/Layout";
 import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css"; //styles of nprogress
+import NProgress from "nprogress"; // nprogress module
+import "nprogress/nprogress.css"; // styles of nprogress
 import { Analytics } from "@vercel/analytics/react";
 
-
-//Route Events.
+// Route Events.
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
