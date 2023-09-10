262f0de5c3e9d28d850ae3c5e7b1101a3889de3e
helges1 <sebhelg@gmail.com>
2023-09-10 18:45:31 +0200

    minor changes

--- components/for-bedrifter/pamelding/pamelding.tsx
+++ components/for-bedrifter/pamelding/pamelding.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function PameldingInfo() {
   const [companyName, setCompanyName] = useState("");
