b2b377d03b5f3f6433d2c95142070aea0d920fec
Sebastian Helgesen <sebhelg@gmail.com>
2024-09-17 01:35:09 +0200

    Rettet skrivefeil

--- pages/for-studenter/karrieredagene/index.tsx
+++ pages/for-studenter/karrieredagene/index.tsx
@@ -10,7 +10,7 @@ const CareerFair: NextPage = () => {
   return (
     <div>
       <HeadSEO
-        title="Karrieredagene 2024 | ITxBERGEN"
+        title="Karrieredagen 2024 | ITxBERGEN"
         description="Informasjon over hva som skjer på karrieredagene til ITxBergen."
         canonical="/for-studenter/karrieredagene"
       />
